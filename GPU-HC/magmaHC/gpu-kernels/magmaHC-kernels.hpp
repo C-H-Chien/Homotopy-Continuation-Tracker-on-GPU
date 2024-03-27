fab2fb72b5db5eecbab5632368ace85f48824ab1
C-H-Chien <cchien3@gpu2003.oscar.ccv.brown.edu>
2024-03-27 17:45:33 -0400

    Add P2C version of trifocal_2op1p_30x30 problem (approach 2) and change target parameters generated from real datasets

--- GPU-HC/magmaHC/gpu-kernels/magmaHC-kernels.hpp
+++ GPU-HC/magmaHC/gpu-kernels/magmaHC-kernels.hpp
@@ -21,35 +21,72 @@
 
 
 //> Geometric Form
-real_Double_t kernel_HC_Solver_5pt_rel_pos_geo_form_quat(
-  magma_queue_t my_queue, \
-  magmaFloatComplex** d_startSols_array, magmaFloatComplex** d_Track_array, \
-  magma_int_t* d_Hx_idx_array,           magma_int_t* d_Ht_idx_array, \
-  magmaFloatComplex_ptr d_phc_coeffs_Hx, magmaFloatComplex_ptr d_phc_coeffs_Ht, \
-  bool* d_is_GPU_HC_Sol_Converge,        bool* d_is_GPU_HC_Sol_Infinity, \
-  magmaFloatComplex* d_Debug_Purpose
+real_Double_t kernel_HC_Solver_5pt_rel_pos_geo_form_quat(                      
+  magma_queue_t         my_queue, 
+  int                   HC_max_steps, 
+  int                   HC_max_correction_steps, 
+  int                   HC_delta_t_incremental_steps,
+  magmaFloatComplex**   d_startSols_array, 
+  magmaFloatComplex**   d_Track_array,
+  magma_int_t*          d_Hx_idx_array,           
+  magma_int_t*          d_Ht_idx_array,
+  magmaFloatComplex_ptr d_phc_coeffs_Hx, 
+  magmaFloatComplex_ptr d_phc_coeffs_Ht,
+  bool*                 d_is_GPU_HC_Sol_Converge,        
+  bool*                 d_is_GPU_HC_Sol_Infinity,
+  magmaFloatComplex*    d_Debug_Purpose
 );
 
 //> Algebraic Form
 real_Double_t kernel_HC_Solver_5pt_rel_pos_alg_form_quat(                      
-  magma_queue_t my_queue, \
-  magmaFloatComplex** d_startSols_array, magmaFloatComplex** d_Track_array, \
-  magma_int_t* d_Hx_idx_array,           magma_int_t* d_Ht_idx_array, \
-  magmaFloatComplex_ptr d_phc_coeffs_Hx, magmaFloatComplex_ptr d_phc_coeffs_Ht, \
-  bool* d_is_GPU_HC_Sol_Converge,        bool* d_is_GPU_HC_Sol_Infinity, \
-  magmaFloatComplex* d_Debug_Purpose
+  magma_queue_t         my_queue, 
+  int                   HC_max_steps, 
+  int                   HC_max_correction_steps, 
+  int                   HC_delta_t_incremental_steps,
+  magmaFloatComplex**   d_startSols_array, 
+  magmaFloatComplex**   d_Track_array,
+  magma_int_t*          d_Hx_idx_array,           
+  magma_int_t*          d_Ht_idx_array,
+  magmaFloatComplex_ptr d_phc_coeffs_Hx, 
+  magmaFloatComplex_ptr d_phc_coeffs_Ht,
+  bool*                 d_is_GPU_HC_Sol_Converge,        
+  bool*                 d_is_GPU_HC_Sol_Infinity,
+  magmaFloatComplex*    d_Debug_Purpose
 );
 
 //> Trifocal Relative Pose from Lines at Points (30x30 Form)
 real_Double_t kernel_HC_Solver_trifocal_2op1p_30x30(
-  magma_queue_t my_queue,
-  magmaFloatComplex** d_startSols_array,  magmaFloatComplex** d_Track_array, \
-  magmaFloatComplex*  d_startParams,      magmaFloatComplex*  d_targetParams, \
-  magmaFloatComplex*  d_diffParams, \
-  magma_int_t* d_Hx_indx, magma_int_t* d_Ht_indx, \
-  bool* d_is_GPU_HC_Sol_Converge,        bool* d_is_GPU_HC_Sol_Infinity, \
-  magmaFloatComplex* d_Debug_Purpose \
+  magma_queue_t         my_queue,
+  int                   HC_max_steps, 
+  int                   HC_max_correction_steps, 
+  int                   HC_delta_t_incremental_steps,
+  magmaFloatComplex**   d_startSols_array,  
+  magmaFloatComplex**   d_Track_array,
+  magmaFloatComplex*    d_startParams,      
+  magmaFloatComplex*    d_targetParams,
+  magmaFloatComplex*    d_diffParams,
+  magma_int_t*          d_Hx_indx, 
+  magma_int_t*          d_Ht_indx,
+  bool*                 d_is_GPU_HC_Sol_Converge,        
+  bool*                 d_is_GPU_HC_Sol_Infinity,
+  magmaFloatComplex*    d_Debug_Purpose
 );
 
+//> Trifocal Relative Pose from Lines at Points (30x30 Form) P2C Version
+real_Double_t kernel_HC_Solver_trifocal_2op1p_30x30_P2C (
+  magma_queue_t         my_queue,
+  int                   HC_max_steps, 
+  int                   HC_max_correction_steps, 
+  int                   HC_delta_t_incremental_steps,
+  magmaFloatComplex**   d_startSols_array, 
+  magmaFloatComplex**   d_Track_array,
+  magma_int_t*          d_Hx_idx_array,
+  magma_int_t*          d_Ht_idx_array,
+  magmaFloatComplex_ptr d_phc_coeffs_Hx,
+  magmaFloatComplex_ptr d_phc_coeffs_Ht,
+  bool*                 d_is_GPU_HC_Sol_Converge,
+  bool*                 d_is_GPU_HC_Sol_Infinity,
+  magmaFloatComplex*    d_Debug_Purpose
+);
 
 #endif
