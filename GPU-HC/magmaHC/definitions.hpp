f348ac69c7c474768e9113d0fdc6aa42d1baee10
C-H-Chien <cchien3@gpu2001.oscar.ccv.brown.edu>
2024-04-11 09:59:31 -0400

    Rolling back to the 100 RANSAC iterations + fastest strategy in evaluation functions

--- GPU-HC/magmaHC/definitions.hpp
+++ GPU-HC/magmaHC/definitions.hpp
@@ -7,7 +7,7 @@
 #define WRITE_FILES_FOLDER                      std::string("Output_Write_Files/")
 
 //> RANSAC Settings
-#define NUM_OF_RANSAC_ITERATIONS                (1)
+#define NUM_OF_RANSAC_ITERATIONS                (100)
 #define IMAG_PART_TOL                           (1e-5)
 #define ROT_RESIDUAL_TOL                        (1e-1)
 #define TRANSL_RESIDUAL_TOL                     (1e-1)
