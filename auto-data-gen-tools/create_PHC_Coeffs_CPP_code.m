ca2facc1998fe86fc815a6041228fbb9fde92189
C-H-Chien <chiangheng.chien@gmail.com>
2025-01-01 23:35:04 -0500

    fix a small error in generating P2C code from the auto-generator tool

--- auto-data-gen-tools/create_PHC_Coeffs_CPP_code.m
+++ auto-data-gen-tools/create_PHC_Coeffs_CPP_code.m
@@ -13,15 +13,18 @@ function max_order_of_func_t = create_PHC_Coeffs_CPP_code(problemName, rep_coeff
     fprintf(outputFileWr_PHC_script, '#include "magma_lapack.h"\n');
     fprintf(outputFileWr_PHC_script, '#include "magma_internal.h"\n');
     fprintf(outputFileWr_PHC_script, '#undef max\n');
-    fprintf(outputFileWr_PHC_script, '#undef min\n\n');
+    fprintf(outputFileWr_PHC_script, '#undef min\n');
+
+    fprintf(outputFileWr_PHC_script, 'namespace magmaHCWrapper {\n');
     
-    fprintf(outputFileWr_PHC_script, "void p2c-");
+    fprintf(outputFileWr_PHC_script, "void p2c_");
     fprintf(outputFileWr_PHC_script, strcat(problemName, "("));
     fprintf(outputFileWr_PHC_script, "magmaFloatComplex *h_targetParams, magmaFloatComplex *h_startParams, magmaFloatComplex *h_phc_coeffs_Hx, magmaFloatComplex *h_phc_coeffs_Ht)\n");
     fprintf(outputFileWr_PHC_script, "{\n");
 
     max_order_of_func_t = createUnivPoly4Coeffs(rep_coeffs_stack, numOfParams, outputFileWr_PHC_script);
 
+    fprintf(outputFileWr_PHC_script, "}\n");
     fprintf(outputFileWr_PHC_script, "}\n");
     fprintf(outputFileWr_PHC_script, "#endif\n");
 end
